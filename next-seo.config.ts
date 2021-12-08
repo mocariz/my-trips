4b57cd62bda33927368566cd733d31fec01ca99b
Monica Mota Ocariz <monica.ocariz@i-value.com.br>
2021-12-08 12:57:24 -0300

    Add seo and loading feedback

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://my-trips.willianjusten.com.br',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@mocariz_',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
