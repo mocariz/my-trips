0c3780b630a0501f9e7021d957e4f47ce05e147d
Monica Mota Ocariz <monica.ocariz@i-value.com.br>
2021-12-08 17:30:43 -0300

    Add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true
+  },
   images: {
     domains: ['media.graphcms.com']
   }
-}
+})
